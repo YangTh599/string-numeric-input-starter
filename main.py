b31ab1b3b3bb90d32ec86c6dc523952b7c4c3681
YangTh599 <YangTh599@northwested.org>
2024-09-13 10:38:20 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,24 +1,42 @@
-# Student name
-# Current date
+# Thayer Yang
+# 13 SEP 2024
 # String & Numeric Input 
 
 # Task 1
-# Comment
-# Comment
+
+first_name = input("What is your first name?\n")
+program = input("What program do you take at CTC?\n")
+
+print(first_name.capitalize()+" is a student at CTC. They are doing "+program)
 
 
 
 
 # Task 2
-# Comment
-# Comment
+
+age = int(input("How old are you?\n")) 
+
+while isinstance(age, int) == False:
+    print("Please enter an integer.")
+    age = int(input("How old are you?\n"))
+    if isinstance(age, int) == True:
+        break
+
+
+print(first_name+" is "+str(age)+" years old. In 10 years they'll be "+str(age+10)+" years old.")
+
 
 
 
 
 # Task 3
-# Comment
-# Comment
+num_of_family = int(input("How many family members do you have?\n"))
+while isinstance(num_of_family, int) == False:
+    print("Please enter an integer.")
+    age = int(input("How many family members do you have?\n"))
+    if isinstance(num_of_family, int) == True:
+        break
+print(f"{first_name} has {str(num_of_family)} family members in their family.")
 
 
 
@@ -27,7 +45,15 @@
 # Comment
 # Comment
 
+cm_constant = 2.54
+height_in = float(input("How tall are you in inches.\n"))
+while isinstance(height_in, int) == False or isinstance(height_in, float) == False:
+    print("Please enter an integer.")
+    height_in = int(input("How many family members do you have?\n"))
+    if isinstance(height_in, int) == True or isinstance(height_in, float) == True:
+        break
 
+print(first_name+" is "+str(height_in)+" inches tall which means they are "+str(cm_constant*height_in)+" cm tall.")
 
 
 

8f0496d528530e06f739a2bfd566190407f162f3
YangTh599 <YangTh599@northwested.org>
2024-09-13 10:46:54 -0400

    Update main.py

--- main.py
+++ main.py
@@ -14,13 +14,16 @@ print(first_name.capitalize()+" is a student at CTC. They are doing "+program)
 
 # Task 2
 
-age = int(input("How old are you?\n")) 
-
-while isinstance(age, int) == False:
-    print("Please enter an integer.")
-    age = int(input("How old are you?\n"))
-    if isinstance(age, int) == True:
+while True:
+    try:
+        age = int(input("How old are you?\n")) 
+    except ValueError: 
+        print("Please enter an integer")
+        continue
+    else:
         break
+
+
 
 
 print(first_name+" is "+str(age)+" years old. In 10 years they'll be "+str(age+10)+" years old.")
@@ -30,12 +33,15 @@ print(first_name+" is "+str(age)+" years old. In 10 years they'll be "+str(age+1
 
 
 # Task 3
-num_of_family = int(input("How many family members do you have?\n"))
-while isinstance(num_of_family, int) == False:
-    print("Please enter an integer.")
-    age = int(input("How many family members do you have?\n"))
-    if isinstance(num_of_family, int) == True:
+while True:
+    try:
+        num_of_family = int(input("How many family members do you have?\n"))
+    except ValueError: 
+        print("Please enter an integer") 
+        continue
+    else:
         break
+
 print(f"{first_name} has {str(num_of_family)} family members in their family.")
 
 
@@ -46,11 +52,13 @@ print(f"{first_name} has {str(num_of_family)} family members in their family.")
 # Comment
 
 cm_constant = 2.54
-height_in = float(input("How tall are you in inches.\n"))
-while isinstance(height_in, int) == False or isinstance(height_in, float) == False:
-    print("Please enter an integer.")
-    height_in = int(input("How many family members do you have?\n"))
-    if isinstance(height_in, int) == True or isinstance(height_in, float) == True:
+while True:
+    try:
+        height_in = float(input("How tall are you in inches.\n"))
+    except ValueError:
+        print("Please enter a number")
+        continue
+    else:
         break
 
 print(first_name+" is "+str(height_in)+" inches tall which means they are "+str(cm_constant*height_in)+" cm tall.")
